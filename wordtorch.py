a4b28746c2c9604d25e400f010e32bd7f363599a
lukamircetic <49099101+lukamircetic@users.noreply.github.com>
2019-12-10 23:22:10 -0500

    Add files via upload

---
+++ wordtorch.py
@@ -0,0 +1,74 @@
+import torch
+import torchvision
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import transforms, datasets
+
+train = datasets.MNIST('',train=True,download=True,transform=transforms.Compose([transforms.ToTensor()]))
+test = datasets.MNIST('',train=False,download=True,transform=transforms.Compose([transforms.ToTensor()]))
+
+trainset = torch.utils.data.DataLoader(train,batch_size=10,shuffle=True)
+testset = torch.utils.data.DataLoader(test,batch_size=10,shuffle=True)
+
+class Net(nn.Module):
+    
+    def __init__(self):
+        super().__init__() ##output can be whatever you want but we go with 64
+        self.fc1 = nn.Linear(28*28,64) ##28*28 image = 784 which is input
+        self.fc2 = nn.Linear(64,64)    ##takes prevs output
+        self.fc3 = nn.Linear(64,64)
+        self.fc4 = nn.Linear(64,10) ##10 numbers so 10 possible outputs
+        
+    def forward(self,x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        x = self.fc4(x)
+        
+        return F.log_softmax(x,dim=1)
+        
+        
+net = Net()
+print(net)
+
+X = torch.rand(28,28)
+X = X.view(-1,28*28)  ##negative one means input will be unknown shape -1 and 1 gives exact same answer
+output = net(X)
+
+
+
+optimizer = optim.Adam(net.parameters(),lr=0.001) ##size of step of gradient
+
+
+EPOCHS = 3
+for epoch in range(EPOCHS):
+    for data in trainset:
+        #data is a bunch of feature sets and labels
+        X,y = data
+        net.zero_grad()
+        output = net(X.view(-1,28*28))
+        loss = F.nll_loss(output,y)
+        loss.backward() ##back propagation
+        optimizer.step()
+    print(loss)
+
+correct = 0
+total = 0
+
+with torch.no_grad():
+    for data in trainset:
+        X, y = data
+        output = net(X.view(-1,784))
+        for idx, i in enumerate(output):
+            if torch.argmax(i) == y[idx]:
+                correct += 1
+            total += 1
+print("Accuracy: ", round(correct/total, 3))
+
+
+import matplotlib.pyplot as plt
+plt.imshow(X[3].view(28,28))
+plt.show()
+
+print(torch.argmax(net(X[3].view(-1,784))[0]))
